09ff82d31648196384d62e118b0f3f62d967464c
Giovanni Opromolla <giopromolla@gmail.com>
2015-06-21 00:54:05 -0300

    Cliente

---
+++ picoDB/client/ClientSocket.h
@@ -0,0 +1,45 @@
+
+#ifndef CLIENTSOCKET_H_
+#define CLIENTSOCKET_H_
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string>
+
+class ClientSocket {
+
+public:
+
+	ClientSocket();
+	~ClientSocket();
+
+	void conectar(const std::string& host, unsigned int port);
+
+	/**
+	 * Retorna true si el Socket es valido, false en caso contrario.
+	 */
+	bool es_valido();
+
+	void desconectar();
+	void shutdown();
+
+	int recibirMensaje(void* stream,unsigned int size);
+	int enviar(const char* stream,unsigned int size);
+
+	int getFD();
+
+private:
+	int fd; 	 // file descriptor
+	bool valido; // estado del Socket
+	struct sockaddr_in servidor;
+
+};
+
+#endif /* CLIENTSOCKET_H_ */
---
+++ picoDB/shared/src/SocketException.h
@@ -0,0 +1,28 @@
+#ifndef COMMON_SOCKETEXCEPTION_H_
+#define COMMON_SOCKETEXCEPTION_H_
+
+#include <string>
+
+class SocketException{
+
+public:
+
+	SocketException(const std::string &mensaje) : mensaje(mensaje) {
+
+	};
+
+	~SocketException() {};
+
+	std::string getMensaje() const {
+
+		return mensaje;
+
+	};
+
+private:
+
+	std::string mensaje;
+
+};
+
+#endif /* COMMON_SOCKETEXCEPTION_H_ */
