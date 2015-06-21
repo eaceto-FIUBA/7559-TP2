c6dc6bd50d4a4113147b48048a2321e7b217a389
Giovanni Opromolla <giopromolla@gmail.com>
2015-06-21 16:35:13 -0300

    insert into personserver/picoServer.cpp

--- picoDB/shared/inc/person.h
+++ picoDB/shared/inc/person.h
@@ -11,16 +11,25 @@
 #define TABLE_PERSON_COLUMN_TELEFONO_FIXED_SIZE     13
 
 #include <iostream>
+#include "LockFile.h"
+
 using namespace std;
 
 class Person {
 private:
+
+	LockFile file;
+
     char nombre[TABLE_PERSON_COLUMN_NOMBRE_FIXED_SIZE];
     char direccion[TABLE_PERSON_COLUMN_DIRECCION_FIXED_SIZE];
     char telefono[TABLE_PERSON_COLUMN_TELEFONO_FIXED_SIZE];
 
 public:
-    Person(std::string nombre, std::string direccion, std::string telefono);
+    Person(std::string path);
+    ~Person();
+    void addPerson(std::string nombre, std::string direccion, std::string telefono);
+    std::string getPersons();
+//    Person(std::string nombre, std::string direccion, std::string telefono);
 };
 
 #endif //PICODB_PERSON_H
--- picoDB/shared/src/person.cpp
+++ picoDB/shared/src/person.cpp
@@ -2,11 +2,24 @@
 // Created by kimi on 14/06/15.
 //
 
-#include "../inc/person.h"
+#include "person.h"
 
-Person::Person(std::string n, std::string d, std::string t) {
+Person::Person(std::string path): file(path){
 
-    if (LOG) {
+
+}
+
+Person::~Person(){
+
+}
+
+void Person::addPerson(std::string n, std::string d, std::string t) {
+	std::string buffer = n+","+d+","+t+"\n";
+	file.tomarLockEscritura();
+	file.escribir(buffer.c_str(),buffer.size());
+	file.liberarLock();
+
+	if (LOG) {
         cout << "Creating object. {" << n << "\t" << d << "\t" << t << "}" << std::endl;
     }
 
@@ -18,4 +31,9 @@ Person::Person(std::string n, std::string d, std::string t) {
 
     if (t.length() > TABLE_PERSON_COLUMN_TELEFONO_FIXED_SIZE)
         throw ("telefono: " + t + " size " + to_string(t.length()) + " > " + to_string(TABLE_PERSON_COLUMN_TELEFONO_FIXED_SIZE));
+}
+
+std::string Person::getPersons() {
+	std::string total = "";
+	return total;
 }
