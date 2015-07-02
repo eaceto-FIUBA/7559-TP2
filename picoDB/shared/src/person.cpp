aa9f4ece3613aeefbd2fe08b81de9c8b1e9c3427
Kimi <ezequiel.aceto@gmail.com>
2015-07-02 00:42:28 -0300

    no escribe en DB si se viola una restricción

--- picoDB/shared/src/person.cpp
+++ picoDB/shared/src/person.cpp
@@ -14,11 +14,6 @@ Person::~Person(){
 }
 
 bool Person::addPerson(std::string n, std::string d, std::string t) {
-	std::string buffer = n+","+d+","+t+"\n";
-	file.tomarLockEscritura();
-	file.escribir(buffer.c_str(),buffer.size());
-	file.liberarLock();
-
 	if (LOG) {
         cout << "Creating object. {" << n << "\t" << d << "\t" << t << "}" << std::endl;
     }
@@ -31,6 +26,11 @@ bool Person::addPerson(std::string n, std::string d, std::string t) {
 
     if (t.length() > TABLE_PERSON_COLUMN_TELEFONO_FIXED_SIZE)
         throw ("telefono: " + t + " size " + to_string(t.length()) + " > " + to_string(TABLE_PERSON_COLUMN_TELEFONO_FIXED_SIZE));
+
+	std::string buffer = n+","+d+","+t+"\n";
+	file.tomarLockEscritura();
+	file.escribir(buffer.c_str(),buffer.size());
+	file.liberarLock();
 
 	return true;
 }
